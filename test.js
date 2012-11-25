aee34b9680243d9e69d2032d785d526f1114004c
Paul Vorbach <paul@vorb.de>
2012-11-25 12:58:50 +0100

    add test for random integers

--- test.js
+++ test.js
@@ -5,4 +5,14 @@ describe('MD5', function () {
   it('should return the expected MD5 hash for "message"', function () {
     assert.equal('78e731027d8fd50ed642340b7c9a63b3', md5('message'));
   });
+
+  it('should not return the same hash for random numbers twice', function () {
+    var msg1 = Math.floor((Math.random() * 100000) + 1) + (new Date).getTime();
+    var msg2 = Math.floor((Math.random() * 100000) + 1) + (new Date).getTime();
+
+    if (msg1 !== msg2)
+      assert.notEqual(md5(msg1), md5(msg2));
+    else
+      assert.equal(md5(msg1), md5(msg1));
+  });
 });
