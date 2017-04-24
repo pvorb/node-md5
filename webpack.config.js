56de05668aea5b8828559174e5f8611855ee2e6d
caub <cyril.auburtin@gmail.com>
2017-04-24 22:55:04 +0200

    changes requested

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,13 @@
 const {resolve} = require('path');
-const webpack = require('webpack');
 
 module.exports = {
-	entry: [
-		'./md5.js'
-	],
-	output: {
-		path: resolve(__dirname, 'dist'),
-		filename: 'md5.min.js',
-		libraryTarget: "var",
-		library: "MD5"
-	},
-	plugins: [
-		new webpack.optimize.UglifyJsPlugin({
-			compress: {
-				warnings: true
-			},
-			output: {
-				comments: false
-			}
-		})
-	],
+  entry: [
+    './md5.js'
+  ],
+  output: {
+    path: resolve('./dist'),
+    filename: 'md5.min.js',
+    libraryTarget: "var",
+    library: "MD5"
+  }
 };
